8de3f4ab5b21cfcd0676fd7fd0c92b29b9f3f2e4
Mahmoud Khaled <mahmoud.khaled@incorta.com>
2020-04-09 12:13:54 +0200

    clean foo file

--- stress.rb
+++ stress.rb
@@ -4,7 +4,7 @@ interval = 70
 threads = []
 
 1.upto experiments do |i|
-  File.write('foo.txt'. '')
+  File.write('foo.txt', '')
   threads << Thread.new do
     sleep (i-1) * interval
     puts `rspec`
